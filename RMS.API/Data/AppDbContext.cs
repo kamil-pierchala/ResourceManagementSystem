def15ca28ca7b7938b251b8efba00fe6c9319427
Kamil Pierchała <kj.pierchala@gmail.com>
2026-01-31 13:46:22 +0100

    Konfiguracja EF Core, utworzenie modeli i migracja bazy danych

---
+++ RMS.API/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RMS.API.Models;
+
+namespace RMS.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Resource> Resources { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
