e968b7bb858a6670040fd7351c58d4474abaca9e
Kamil Pierchała <kj.pierchala@gmail.com>
2026-01-31 14:15:10 +0100

    Implementacja kontrolerów Users i Resources oraz testy w Swaggerze

---
+++ RMS.API/Controllers/ResourcesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RMS.API.Data;
+using RMS.API.Models;
+
+namespace RMS.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResourcesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ResourcesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Resource>>> GetResources()
+        {
+            return await _context.Resources.Include(r => r.Owner).ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Resource>> PostResource(Resource resource)
+        {
+            _context.Resources.Add(resource);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetResources), new { id = resource.Id }, resource);
+        }
+    }
+}
---
+++ RMS.API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RMS.API.Data;
+using RMS.API.Models;
+
+namespace RMS.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UsersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetUsers), new { id = user.Id }, user);
+        }
+    }
+}
